4f764a284bb3f7ea86387d9cf9b2debe2a36c2a2
Cedric Guillemet <ceguille@microsoft.com>
2021-07-11 18:43:37 +0200

    read back test

--- src/App/Source/App.cpp
+++ src/App/Source/App.cpp
@@ -8,6 +8,31 @@
 #include <glm/vec4.hpp> // glm::vec4
 #include <glm/mat4x4.hpp> // glm::mat4
 
+float canardConvertFloat16ToNativeFloat(uint16_t value)
+{
+	//CANARD_ASSERT(sizeof(float) == 4);
+
+	union FP32
+	{
+		uint32_t u;
+		float f;
+	};
+
+	const union FP32 magic = { (254UL - 15UL) << 23 };
+	const union FP32 was_inf_nan = { (127UL + 16UL) << 23 };
+	union FP32 out;
+
+	out.u = (value & 0x7FFFU) << 13;
+	out.f *= magic.f;
+	if (out.f >= was_inf_nan.f)
+	{
+		out.u |= 255UL << 23;
+	}
+	out.u |= (value & 0x8000UL) << 16;
+
+	return out.f;
+}
+
 namespace Imog3n
 {
 	struct PosColorVertex
@@ -91,8 +116,10 @@ public:
 		m_viewInfosUniform = bgfx::createUniform("viewInfos", bgfx::UniformType::Vec4);
 		m_cameraViewUniform = bgfx::createUniform("cameraView", bgfx::UniformType::Mat4);
 		m_depthIdSampler = bgfx::createUniform("depthIdSampler", bgfx::UniformType::Sampler);
-
+		m_depthIdTexture = CreateReadBackTexture(m_width, m_height);
 		m_depthIdFrameBuffer = CreateDepthIdFrameBuffer(m_width, m_height);
+
+		m_readBackBits = new uint16_t[m_width * m_height * 4 * 2];
 	}
 
 	virtual int shutdown() override
@@ -108,7 +135,7 @@ public:
 	bgfx::FrameBufferHandle CreateDepthIdFrameBuffer(uint16_t width, uint16_t height)
 	{
 		std::array<bgfx::TextureHandle, 2> textures{
-			   bgfx::createTexture2D(width, height, false/*generateMips*/, 1, bgfx::TextureFormat::RGBA16F, BGFX_TEXTURE_RT),
+			   bgfx::createTexture2D(width, height, false/*generateMips*/, 1, bgfx::TextureFormat::RGBA16F, BGFX_TEXTURE_RT ),
 			   bgfx::createTexture2D(width, height, false, 1, bgfx::TextureFormat::D24S8, BGFX_TEXTURE_RT) };
 		std::array<bgfx::Attachment, textures.size()> attachments{};
 		for (size_t idx = 0; idx < attachments.size(); ++idx)
@@ -119,8 +146,27 @@ public:
 		return frameBufferHandle;
 	}
 
+	bgfx::TextureHandle CreateReadBackTexture(uint16_t width, uint16_t height)
+	{
+		auto texture = bgfx::createTexture2D(width, height, false/*generateMips*/, 1, bgfx::TextureFormat::RGBA16F, BGFX_TEXTURE_READ_BACK);
+		return texture;
+	}
+
+	void GetTextureBits(uint16_t width, uint16_t height, uint32_t x, uint32_t y, float* values)
+	{
+		auto sourceTextureHandle = bgfx::getTexture(m_depthIdFrameBuffer);
+		bgfx::blit(4, m_depthIdTexture, 0, 0, sourceTextureHandle);
+		bgfx::readTexture(m_depthIdTexture, m_readBackBits);
+		uint16_t* ptr = m_readBackBits + y * width * 4 + x * 4;
+		*values++ = canardConvertFloat16ToNativeFloat(*ptr++);
+		*values++ = canardConvertFloat16ToNativeFloat(*ptr++);
+		*values++ = canardConvertFloat16ToNativeFloat(*ptr++);
+		*values++ = canardConvertFloat16ToNativeFloat(*ptr++);
+	}
+
 	bool update() override
 	{
+		static float res[4] = {0.f, 0.f, 0.f, 0.f};
 		if (!entry::processEvents(m_width, m_height, m_debug, m_reset, &m_mouseState) )
 		{
 			imguiBeginFrame(m_mouseState.m_mx
@@ -147,6 +193,7 @@ public:
 				, 0
 				);
 
+			ImGui::InputFloat4("V: ", res);
 			ImGui::Text("Primitive topology:");
             if (ImGui::Button("Fit Nodes"))
             {
@@ -200,6 +247,8 @@ public:
 			bgfx::setTexture(0, m_depthIdSampler, textureHandle);
 			bgfx::submit(2, m_DepthId2ColorProgram);
 
+			
+			GetTextureBits(m_width, m_height, m_mouseState.m_mx, m_mouseState.m_my, res);
 
 			// Advance to next frame. Rendering thread will be kicked to
 			// process submitted rendering primitives.
@@ -231,11 +280,15 @@ public:
 	bgfx::UniformHandle m_depthIdSampler;
 
 	bgfx::FrameBufferHandle m_depthIdFrameBuffer;
+	bgfx::TextureHandle m_depthIdTexture;
 
 	float m_cameraViewMatrix[16] = {1.f, 0.f, 0.f, 0.f,
 								0.f, 1.f, 0.f, 0.f,
 								0.f, 0.f, 1.f, 0.f,
 								0.f, 0.f, 2.f, 1.f};
+
+
+	uint16_t* m_readBackBits;
 };
 
 } // namespace
